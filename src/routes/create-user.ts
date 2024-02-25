927bb2432c4273ac1c33124e4c57cbc4fffdd9d2
Lucas <lucasbiazonpalma@gmail.com>
2024-02-25 20:38:38 -0300

    feature:create-user

--- src/routes/create-user.ts
+++ src/routes/create-user.ts
@@ -1,3 +1,29 @@
-export async function CreateUser() {
-  
+import { FastifyInstance } from 'fastify';
+import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export async function CreateUser(server: FastifyInstance) {
+  server.post('/create-user', async (request, reply) => {
+    try{
+      const {email, password} = request.body as { email: string; password: string };
+      if (!email || !password) {
+        reply.status(400).send({ message: 'Email e senha são obrigatórios' });
+        return;
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await prisma.user.create({
+        data:{
+          email,
+          password: hashedPassword
+        }
+       
+      });
+ 
+      return reply.status(201).send({ message: 'Usuário criado com sucesso' });
+    }catch(err){
+      console.error('Erro ao criar usuário:', err);
+      reply.status(500).send({ message: 'Erro ao criar usuário' });
+    }
+  })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
 import {CreateUser} from "./routes/create-user"
 import { LoginUser } from "./routes/login-user";
 import { LogoutUser } from "./routes/logout-user";
 import { UpdateUser } from "./routes/update-information-user";
 import { DeleteUser } from "./routes/delete-user";
 
+
 const server = fastify();
 server.get('/', async (request, reply) => {
   return 'Hello, world JWT!'
 })
 
+
+server.register(fastifyJwt,{secret: 'mysecrete'} )
 
 server.register(CreateUser)
 server.register(LoginUser)
